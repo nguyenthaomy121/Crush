998fe03e39baf7fe27bdd2b93f78cd61c1bf791b
nguyenthaomy121 <hsvd.nguyenthaomy@gmail.com>
2025-05-21 19:30:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Gửi crush của tớ",
-    introTitle: 'Crush yêu dấu',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Gửi Phốc của tmy",
+    introTitle: 'Phốc yêu dấu',
+    introDesc: `Từ lúc có anh thế giới như có thêm màu mới
+    Anh biết anh cười đẹp lắm ôn
+    Cười quài cho em
+    Cho dù em có như thế nào anh vẫn chấp nhận em!?`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    title: 'Anh thật sự rất tốt, em còn không dám sẽ là người được anh yêu thương 🥰',
     desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'Yes,i do <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
+    messDesc: 'Làm bạn trai mình nha, mình chốt bạn rồi!!.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://facebook.com/dinhkaito' //link mess của các bạn. VD: /dinhkaito
-}
+    
